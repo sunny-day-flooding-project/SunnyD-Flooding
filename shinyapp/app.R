8bb480480b5fa1b4085e0c253926ba31ed738cb4
Ian Hoyt-McCullough <ianiac@email.unc.edu>
2024-02-13 14:43:39 -0500

    Fix for camera location colors on map view

--- shinyapp/app.R
+++ shinyapp/app.R
@@ -1159,18 +1159,22 @@ server <- function(input, output, session) {
                                 labelOptions = labelOptions(direction = "top", style=list("border-radius" = "10px")),
                                 layerId = camera_locations$camera_ID,
                                 color = "black",
-                                fillColor = "#28a745",
+                                fillColor = sensor_locations %>%
+                                  left_join(isolate(map_flood_status_reactive()), by = "sensor_ID") %>%
+                                  left_join(tibble("flood_status" = c("UNKNOWN","FLOODING", "WARNING", "NOT FLOODING"),
+                                                    "flood_color" = c("#28a745", "#28a745", "#28a745", "#28a745")), by = "flood_status") %>% 
+                                  pull(flood_color),
                                 fillOpacity = 1.0
                             ) %>%
                              # leaflet::addLegend('bottomright', pal = pal_rev, values = c(-3.5,0.5),
                              #                    title = 'Water level<br>relative to<br>surface (ft)',
                              #                    opacity = 1,
                              #                    labFormat = labelFormat(transform = function(x) sort(x, decreasing = TRUE))) %>% 
-                             leaflet::addLegend('bottomright',
-                                                title = "Flood status",
-                                                colors = c("grey","#dc3545","#ffc107","#28a745"),
-                                                labels = c("Unknown","Flooding", "Warning", "Not Flooding"),
-                                                opacity = 1) %>% 
+                            #  leaflet::addLegend('bottomright',
+                            #                     title = "Flood status",
+                            #                     colors = c("grey","#dc3545","#ffc107","#28a745"),
+                            #                     labels = c("Unknown","Flooding", "Warning", "Not Flooding"),
+                            #                     opacity = 1) %>% 
                              addLayersControl(
                                baseGroups = c("Positron (default)", "Dark Matter","Imagery", "OSM"),
                                options = layersControlOptions(collapsed = T)) %>% 
@@ -1281,6 +1285,7 @@ server <- function(input, output, session) {
     if(input$map_layers == 1){
       leafletProxy(mapId = "m") %>% 
         clearGroup("camera_site") %>% 
+        clearControls() %>%
         addCircleMarkers(data = sensor_locations %>% 
                            left_join(isolate(map_flood_status_reactive()), by = "sensor_ID"), group = "sensor_site",
                          # popup = ~html_popups,
@@ -1295,12 +1300,18 @@ server <- function(input, output, session) {
                            left_join(tibble("flood_status" = c("UNKNOWN","FLOODING", "WARNING", "NOT FLOODING"),
                                             "flood_color" = c("grey", "#dc3545", "#ffc107", "#28a745")), by = "flood_status") %>% 
                            pull(flood_color),
-                         fillOpacity = 1) 
+                         fillOpacity = 1) %>%
+                         leaflet::addLegend('bottomright',
+                                                title = "Flood status",
+                                                colors = c("grey","#dc3545","#ffc107","#28a745"),
+                                                labels = c("Unknown","Flooding", "Warning", "Not Flooding"),
+                                                opacity = 1)
     }
     
     if(input$map_layers == 2){
       leafletProxy(mapId = "m") %>% 
         clearGroup("sensor_site") %>% 
+        clearControls() %>%
         # addAwesomeMarkers(data = camera_locations, icon=map_icon, group = "camera_site",
         #                   label = lapply(camera_locations_labels,HTML),
         #                   labelOptions = labelOptions(direction = "top", style=list("border-radius" = "10px")),
